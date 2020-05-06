0cf1497a7746aa3ccb54c25578001c2440abc305
Shubhamsm <shubhammehraxc@gmail.com>
2020-05-07 02:30:07 +0530

    hello

--- 2nd-way-selenium.py
+++ 2nd-way-selenium.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import  Keys
 import time ,os
 
-os.chdir(r"/home/shubham/Documents/new/project/youtube_auto_comments/youtube-auto-comments/using selenium #2/")
+os.chdir(r"/home/shubham/Documents/new/project/Github/youtube-auto-comments/")
 
 driver = webdriver.Chrome(executable_path="./chromedriver")
 username=""         # TODO
@@ -13,9 +13,13 @@ password=""         # TODO
 
 driver.get("http://www.youtube.com")       # open Youtube
 time.sleep(6)
-pyautogui.hotkey("ctrl","l")
+pyautogui.hotkey("ctrl","l")    ; time.sleep(2)
 pyautogui.write('https:/www.youtube.com/results?search_query=hello')
 pyautogui.press("enter")
+time.sleep(5)
+
+element=driver.find_element_by_xpath('//*[@id="video-title"]/yt-formatted-string')
+element.click()
 
 
 # driver.quit()
